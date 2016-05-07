6a0fad843d23c77bea3886b1c6ed5e576905fdb5
onncho <onncho@Onns-MacBook-Pro.local>
2016-05-07 10:13:08 +0300

    a

--- TotoTrack/www/js/dashboardCharts.js
+++ TotoTrack/www/js/dashboardCharts.js
@@ -1,3 +1,39 @@
 /**
  * Created by onncho on 5/7/16.
  */
+app.factory('loginSrv', ["$http",'Backand', function($http, Backand) {
+        var service = {};
+
+        service.loginBackand = function(cerdentials) {
+            return $http ({
+                method: 'GET',
+                url: Backand.getApiUrl() + '/1/query/data/intersectUsernamePassword',
+                params: {
+                    parameters: {
+                        username: cerdentials.username,
+                        password: cerdentials.password
+                    }
+                }
+            })
+        }
+
+        return service;
+
+    }])
+    .controller('LoginCtrl',['$scope','$state','loginSrv', function ($scope, $state, loginSrv) {
+        var model = {};
+
+        $scope.login = function(data) {
+            loginSrv.loginBackand(data).then(function(response) {
+                if(response.data.length > 0) {
+                    debugger;
+                    model.isSuccess = true;
+                    $state.go('app.dashboard');
+                }else {
+                    model.isSuccess = false;
+                }
+            })
+        }
+
+        $scope.model = model;
+    }])
